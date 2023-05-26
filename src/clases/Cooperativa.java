383a19096e2fc6dd2c9cd06bf19e9279cca31fd3
Emiliano <verde_natural2003@hotmail.com>
2023-05-26 18:38:40 -0500

    Creo la cooperativa}

---
+++ src/clases/Cooperativa.java
@@ -0,0 +1,10 @@
+package clases;
+import java.util.ArrayList;
+
+
+public class Cooperativa extends Usuario{
+	String _especializacion;
+	ArrayList<Cooperativa> _asociaciones = new ArrayList<Cooperativa>();
+	
+	
+}
